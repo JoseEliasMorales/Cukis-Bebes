caf7f07f74564f4673f454ce1d3cafa83bcf4806
Joselias <joselias.dev@gmail.com>
2023-08-01 11:18:40 -0300

    feat: catalog cards

---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Items from '../components/Items'
+
+const Catalog = () => {
+  return (
+    <div>
+      <Items />
+    </div>
+  )
+}
+
+export default Catalog
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Items from '../components/Items'
 import '../styles/home.css'
 
 const Home = () => {
   return (
     <div>
       Hola
+      <Items limit={6} />
     </div>
   )
 }
