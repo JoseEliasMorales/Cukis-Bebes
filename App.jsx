3f9a535f3f5db520d8e0031e975d0b6d59075739
Joselias <joselias.dev@gmail.com>
2023-08-02 12:13:43 -0300

    feat: footer

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import Home from './src/pages/Home'
 import './src/styles/app.css'
 import Navbar from './src/containers/Navbar'
 import Catalog from './src/pages/Catalog'
+import Footer from './src/containers/Footer'
 export function App () {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ export function App () {
         <Route path='/' element={<Home />} />
         <Route path='/catalog' element={<Catalog />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,17 @@
+import Contact from '../components/Contact'
+import FormFooter from '../components/FormFooter'
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+    <section className='footer'>
+      <div className='containerFooter'>
+        <Contact />
+        <FormFooter />
+      </div>
+      <p className='rigths'>Todos los derechos reservados</p>
+    </section>
+  )
+}
+
+export default Footer
