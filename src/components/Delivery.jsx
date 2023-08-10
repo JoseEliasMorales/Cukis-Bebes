407fa96580b0a44221d23230826bc31479f33e2d
Joselias <joselias.dev@gmail.com>
2023-08-10 19:20:40 -0300

    fix: image 6

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import '../styles/delivery.css'
+import confirmation from '../assets/confirmation1.png'
+import clothing from '../assets/babyClothing1.png'
+import box from '../assets/box1.png'
 
 function Delivery () {
   return (
     <div className='contDelivery'>
       <div className='delivery'>
         <h3 className='titleDelivery'>Entregas personales</h3>
-        <img src='../../public/img/confirmation1.png' alt='' />
+        <img src={confirmation} alt='Entregas personales' />
       </div>
       <div className='separador' />
       <div className='delivery'>
         <h3 className='titleDelivery'>Ropa hecha a mano</h3>
-        <img src='../../public/img/babyClothing1.png' alt='' />
+        <img src={clothing} alt='Ropa hecha a mano' />
       </div>
       <div className='separador' />
       <div className='delivery'>
         <h3 className='titleDelivery'>Compra totalmente segura</h3>
-        <img src='../../public/img/box1.png' alt='' style={{ height: '262px' }} />
+        <img src={box} alt='Compra totalmente segura' style={{ height: '262px' }} />
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,17 @@ import CardsNosContainer from '../containers/CardsNosContainer'
 import SobreNosotros from '../components/SobreNosotros'
 import Delivery from '../components/Delivery'
 import BannerHome from '../components/BannerHome'
+import globos1 from '../assets/imagenGlobos1.png'
+import globos2 from '../assets/imagenGlobos2.png'
 
 const Home = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <BannerHome />
       <div className='contGlobos'>
-        <img src='../../public/img/imagenGlobos1.png' alt='' style={{ height: '179px' }} />
+        <img src={globos1} alt='' style={{ height: '179px' }} />
         <SobreNosotros />
-        <img src='../../public/img/imagenGlobos2.png' alt='' style={{ height: '221px', marginTop: '85px' }} />
+        <img src={globos2} alt='' style={{ height: '221px', marginTop: '85px' }} />
       </div>
       <CardsNosContainer />
       <h2 className='subtitle'>Productos en tendencia</h2>
