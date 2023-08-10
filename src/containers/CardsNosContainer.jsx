8b654d21aad95cdc9907ac885e4cb67acd2dfd45
Joselias <joselias.dev@gmail.com>
2023-08-10 19:27:30 -0300

    fix: image 7

--- src/containers/CardsNosContainer.jsx
+++ src/containers/CardsNosContainer.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import '../styles/home.css'
 import CardsNosotros from '../components/CardsNosotros'
+import card1 from '../assets/imageCard1.png'
+import card2 from '../assets/imageCard2.png'
+import card3 from '../assets/imageCard3.png'
 
 const cardData = [
   {
     title: 'Misión',
     p: 'Nuestra misión es poder brindarte una amplia variedad de prendas adorables, suaves y de alta calidad, siendo cada una única y distinta.',
-    image: '../public/img/imageCard1.png'
+    image: card1
   },
   {
     title: 'Visión',
     p: 'Nuestra visión es convertirnos en tu opción preferida a la hora de elegir ropa y accesorios para tus hijos, llegando a cada día a más hogares.',
-    image: '../public/img/imageCard2.png'
+    image: card2
   },
   {
     title: 'Valores',
     p: 'Nuestro valores se basan en la originalidad, en el amor y en el cuidado. Buscando prendas que acompañen a tu bebe en sus primeros pasos.',
-    image: '../public/img/imageCard3.png'
+    image: card3
   }
 ]
 
