f2d3f4ada17e252d2b0497e94d54c147c55c9a7d
Franco Tevez <tevezzfranco@gmail.com>
2023-08-11 14:42:56 -0300

    feat: mod images home page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,23 +2,27 @@ import '../styles/contact.css'
 import whats from '../assets/whatsFooter.png'
 import insta from '../assets/instaFooter.png'
 import face from '../assets/faceFooter.png'
+import logo from '../assets/logoCukisBebesSmall2.png'
 const Contact = () => {
   return (
     <div className='containerContact'>
       <h3 className='titleContact'>Contactame</h3>
-      <div className='containerDateContact'>
-        <div className='dateContact'>
-          <img src={whats} alt='whatsapp' />
-          <p>12345678910</p>
+      <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+        <div className='containerDateContact'>
+          <div className='dateContact'>
+            <img src={whats} alt='whatsapp' />
+            <p>12345678910</p>
+          </div>
+          <div className='dateContact'>
+            <img src={insta} alt='instagram' />
+            <p>@Cukisbebes</p>
+          </div>
+          <div className='dateContact'>
+            <img src={face} alt='facebook' />
+            <p>Cukis bebes</p>
+          </div>
         </div>
-        <div className='dateContact'>
-          <img src={insta} alt='instagram' />
-          <p>@Cukisbebes</p>
-        </div>
-        <div className='dateContact'>
-          <img src={face} alt='facebook' />
-          <p>Cukis bebes</p>
-        </div>
+        <img src={logo} style={{ height: '211px', width: '254px' }} alt='' />
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,21 @@ import CardsNosContainer from '../containers/CardsNosContainer'
 import SobreNosotros from '../components/SobreNosotros'
 import Delivery from '../components/Delivery'
 import BannerHome from '../components/BannerHome'
-import globos1 from '../assets/imagenGlobos1.png'
-import globos2 from '../assets/imagenGlobos2.png'
+import globos1 from '../assets/imagenCirculos1.png'
+import globos2 from '../assets/imagenCirculos2.png'
+import globos3 from '../assets/imagenCirculos4.png'
+import rayas1 from '../assets/imagenRayas2.png'
 
 const Home = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <BannerHome />
       <div className='contGlobos'>
-        <img src={globos1} alt='' style={{ height: '179px' }} />
+        <img src={globos1} alt='' style={{ height: '293px', marginTop: '122px', position: 'absolute', left: '0' }} />
+        <img src={globos3} alt='' style={{ height: '501px', marginTop: '1185px', position: 'absolute', left: '0', zIndex: '-10' }} />
+        <img src={rayas1} alt='' style={{ height: '293px', marginTop: '985px', position: 'absolute', right: '0', zIndex: '-10' }} />
         <SobreNosotros />
-        <img src={globos2} alt='' style={{ height: '221px', marginTop: '85px' }} />
+        <img src={globos2} alt='' style={{ height: '290px', position: 'absolute', right: '0' }} />
       </div>
       <CardsNosContainer />
       <h2 className='subtitle'>Productos en tendencia</h2>
