5e28581208a6c7059f890ba369e27050e156fa21
Joselias <joselias.dev@gmail.com>
2023-08-30 14:51:24 -0300

    fix questions

--- src/pages/Asks.jsx
+++ src/pages/Asks.jsx
@@ -1,10 +1,12 @@
 import BannerAsks from '../components/BannerAsks'
+import Questions from '../containers/Questions'
 import '../styles/asks.css'
 
 const Asks = () => {
   return (
     <section>
       <BannerAsks />
+      <Questions />
     </section>
   )
 }
