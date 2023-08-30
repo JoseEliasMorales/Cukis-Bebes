472a01808af10d576209424b2536c2935c4b245c
Joselias <joselias.dev@gmail.com>
2023-08-30 19:49:14 -0300

    arreglos varios

--- src/components/BannerSizes.jsx
+++ src/components/BannerSizes.jsx
@@ -1,12 +1,14 @@
 import rayasMedidas from '../assets/rayasMedidas.png'
 import '../styles/bannerSize.css'
+import fanSize from '../assets/fanSize.png'
 
 const BannerSizes = () => {
   return (
     <section className='banner'>
       <h2 className='title'>Conocé nuestras medidas</h2>
       <p className='textoBannerSizes'>Si tenés dudas acerca del talle ¡No dudes en consultarnos!</p>
-      <img src={rayasMedidas} alt='' className='tape' />
+      <img src={rayasMedidas} alt='' className='lines' />
+      <img src={fanSize} alt=' ' className='fanVer' style={{ right: `${window.innerWidth > 1200 ? (window.innerWidth - 993) / 3 - 20 : (window.innerWidth - 768) / 3 + 10}px` }} />
     </section>
   )
 }
