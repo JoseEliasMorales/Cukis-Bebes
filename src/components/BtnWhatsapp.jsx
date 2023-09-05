903e0622f711061d5c0225b874250877e1f05637
Jose Elias Morales <joselias.dev@gmail.com>
2023-09-05 09:35:21 -0300

    Update BtnWhatsapp.jsx

--- src/components/BtnWhatsapp.jsx
+++ src/components/BtnWhatsapp.jsx
@@ -9,7 +9,7 @@ const BtnWhatsapp = () => {
     setHovered(!hovered)
   }
   return (
-    <a href='https://wa.me/c/5493516001513' target='_blank' className='btnWhats' rel='noreferrer' onMouseEnter={handleHover} onMouseLeave={handleHover} rel='noreferrer'>
+    <a href='https://wa.me/c/5493516001513' target='_blank' className='btnWhats' rel='noreferrer' onMouseEnter={handleHover} onMouseLeave={handleHover}>
       <img src={hovered ? whatsBtn : whatsBtnTransparent} alt='' />
       Consultar Precio
     </a>
